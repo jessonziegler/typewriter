f8bda6ce0ccd381e012c3737d6e5e1da8722b2ab
jessonziegler <jziegleraudio@gmail.com>
2021-09-22 07:11:58 +0000

    animated sentence

---
+++ typewriter.js
@@ -0,0 +1,114 @@
+const sentence = "h";
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 0)
+};
+const sentence2 = "e";
+for (const char of sentence2) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 50)
+};
+const sentence3 = "l";
+for (const char of sentence3) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 100)
+};
+const sentence4 = "l";
+for (const char of sentence4) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 150)
+};
+const sentence5 = "o";
+for (const char of sentence5) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 200)
+};
+const sentence6 = "f";
+for (const char of sentence6) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 250)
+};
+const sentence7 = "r";
+for (const char of sentence7) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 300)
+};
+const sentence8 = "o";
+for (const char of sentence8) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 350)
+};
+const sentence9 = "m";
+for (const char of sentence9) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 400)
+};
+const sentence10 = "l";
+for (const char of sentence10) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 450)
+};
+const sentence11 = "i";
+for (const char of sentence11) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 500)
+};
+const sentence12 = "g";
+for (const char of sentence12) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 550)
+};
+const sentence13 = "h";
+for (const char of sentence13) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 600)
+};
+const sentence14 = "t";
+for (const char of sentence14) {
+  setTimeout(() => {
+    process.stdout.write(char);
+}, 650)
+};
+const sentence15 = "h";
+for (const char of sentence15) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 700)
+};
+const sentence16 = "o";
+for (const char of sentence16) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 750)
+};
+const sentence17 = "u";
+for (const char of sentence17) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 800)
+};
+const sentence18 = "s";
+for (const char of sentence18) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 850)
+};
+const sentence19 = "e";
+for (const char of sentence19) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 900)
+};
